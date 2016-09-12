fdf69fd68fa340bae69093cadb90971bf8fea04f
rnwhdghl12@naver.com <rnwhdghl12@naver.com>
2016-09-12 14:14:57 +0900

    메인으로 사용될 기본 구조

--- Head/MTT_MAIN.h
+++ Head/MTT_MAIN.h
@@ -1,48 +1,41 @@
-#include"DetectingModel.h"
 #include"DebugViewer.h"
+#include"LabelController.h"
+#include"FrameController.h"
+#include"Classification.h"
+#include"ExtractBlob.h"
 using namespace std;
 using namespace cv;
-#define DEBUG_LV2
-#define DEBUG_LV1
 #define _MAIN_  int main(){ MAIN m;m.Do();}
 class MAIN{
-public:
-	MAIN(){}
-	void Do(){
-		DetectingModel* DM = DetectingModel::mMakeModel();
-		MarkList* mark = MarkList::mMakeMarkList();
-		Classification* Cl = Classification::mMakeClassify();
-		FrameController* Fc = FrameController::mMakeController();
-#ifdef DEBUG_LV1
-		DebugViewer* DV = DebugViewer::mMakeViewer();
-		DebugController* DC = DebugController::mMakeController();
-		DV->mConnect(MVC::C, DC);
-		DV->mConnect(MVC::M, DM);
-		
-		DM->mConnect(MVC::C, DC);
-		DM->mConnect(MVC::V, DV);
-		
-		DC->mConnect(MVC::M, DM);
-		DC->mConnect(MVC::V, DV);
-#endif
-//		시작 전 기본 데이터 구축(마크 모양)----------------------------
-		vector<Mat> MarkVc;
-		MarkVc.push_back(cv::imread("Event.jpg", cv::IMREAD_COLOR));
-		MarkVc.push_back(cv::imread("Mark_1.jpg", cv::IMREAD_COLOR));
-		MarkVc.push_back(cv::imread("Mark_2.jpg", cv::IMREAD_COLOR));
-			
-		for (size_t i = 0; i < MarkVc.size();++i)
-			mark->mAddMark(MarkVc[i]);
+private:
+	VideoCapture vc;
+	LabelController LbC;
+	FrameController* FrC = FrameController::mMakeController();
+	ExtractBlob exB;
+	Classification* Clf = Classification::mMakeClassify();
+	Mat s;
+	MarkList* Ml = MarkList::mMakeMarkList();
 
-//		-------------------------------------------------------------
-		while (DM->mAction()){
-#ifdef DEBUG_LV1
-			DV->mShowViewer();
-#else if
-			
-#endif
-			Cl->classify();
-			Fc->mControll();
+public:
+	MAIN() :vc("Sample.avi"), LbC(), exB(){
+		FrC = FrameController::mMakeController();
+		Clf = Classification::mMakeClassify();
+		Ml = MarkList::mMakeMarkList();
+	}
+
+
+	void Do(){
+		Ml->mAddMark(imread("Event.jpg"));
+		Ml->mAddMark(imread("Mark_1.jpg"));
+		Ml->mAddMark(imread("Mark_2.jpg"));
+		while (waitKey(10) != 27){
+			vc >> s;
+			imshow("A", s);
+			LbC.mModify(exB(s));
+			Clf->mGetInfo(LbC.mGetVc());
+			Clf->classify();
+			FrC->mControll();
 		}
 	}
 };
+
