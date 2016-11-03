62738accc7af49d6d1f396d65cd25a7a05bdc4b9
rnwhdghl12@naver.com <rnwhdghl12@naver.com>
2016-11-04 00:14:04 +0900

    기존의 내용 오류 수정 // MID -> markID , LID 만 등록하던 문제 LID, markID 등록으로 변경

--- JavaCode/DATA/Label.java
+++ JavaCode/DATA/Label.java
@@ -4,17 +4,17 @@ import java.awt.Point;
 
 public class Label {
 	Point p;
-	int LID,MID,PID;
+	int LID,markID,PID;
+	//xxxxyyyLIDmI
 	public Label(String info){
 		p = new Point(Integer.parseInt(info.substring(0, 4)),Integer.parseInt(info.substring(4, 7)));
 		LID = Integer.parseInt(info.substring(7, 9));
-		LID = Integer.parseInt(info.substring(9, 11));
-		LID = Integer.parseInt(info.substring(11,12));
+		markID = Integer.parseInt(info.substring(9, 11));
 	}
 	public Label(){
 		p=new Point(0,0);
 		LID = 1;
-		MID = 1;
+		markID = 1;
 	}
 	public Point GetPT(){return p;}
 	public void SetPID(int p){PID = p;}
