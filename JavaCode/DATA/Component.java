b33ea5bcf63fe396537d3aa696836893167951fb
rnwhdghl12@naver.com <rnwhdghl12@naver.com>
2016-11-16 19:51:39 +0900

    기존의 component 개념은 Model의 하위 개념이였으나 Model의 상위 개념으로 모델의 data로 바꿈

--- JavaCode/DATA/Component.java
+++ JavaCode/DATA/Component.java
@@ -1,19 +1,91 @@
-package DATA;
+package data;
 
 import java.awt.Point;
-import java.util.Random;
+import java.util.Comparator;
+import java.util.Observable;
 
-import DATAMethod.RandomID;
+import datamethod.ComponentController;
+import datamethod.RandomID;
 
-public class Component {
-	int ID;
-	Point LT;
-	public Component(){
-		LT=new Point(0,0);
-		ID = RandomID.MakeID(this);
+public abstract class Component extends Observable {
+	protected int ID;
+	private int z;
+    private Rect area;
+    private Point lt,rb;
+    final public boolean isContain(Point pt) {
+		if(lt.x<pt.x&&pt.x<rb.x &&
+			lt.y<pt.y&&pt.y<rb.y)
+			return true;
+		return false;
 	}
-	public int GetID(){return ID;}
-	public void SetLoc(Point pt){LT=pt;}
-	public void SetLoc(int x,int y){LT=new Point(x,y);}
-	public Point GetLoc(){return LT;}
+    final public void setZ(int _z) {
+    	z=_z;
+    }
+    final public int ID(){
+    	return ID;
+    }
+    final public void setLT(Point _lt) {
+    	lt.setLocation(_lt);
+    	area.setH(rb.y-lt.y);
+    	area.setW(rb.x-lt.x);
+    }
+    final public void setLT(int x,int y) {
+    	lt.setLocation(x,y);
+    	area.setH(rb.y-lt.y);
+    	area.setW(rb.x-lt.x);
+    }
+    final public void setRB(Point _rb) {
+    	rb.setLocation(_rb);
+    	area.setH(rb.y-lt.y);
+    	area.setW(rb.x-lt.x);
+    }
+    final public void setRB(int x,int y) {
+    	rb.setLocation(x,y);
+    	area.setH(rb.y-lt.y);
+    	area.setW(rb.x-lt.x);
+    }
+    public int size() {
+    	return area.size();
+    }
+	public Point lt() {
+		return lt;
+	}
+	public Point rb() {
+		return rb;
+	}
+	final protected void throwLabel(){
+		setChanged();
+		notifyObservers();
+	}
+	public Component(Point lt,int w, int h){
+		z = 1000000;
+    	ComponentController.goTop(this);
+    	ID = RandomID.makeID(this);
+    	area = new Rect(w,h);
+	}
+	public Component(Point lt,Point rb){
+		z = 1000000;
+    	ComponentController.goTop(this);
+    	ID = RandomID.makeID(this);
+    	area = new Rect(lt,rb);
+	}
+	
+    public Component(int x,int y,int w,int h) {
+    	z = 1000000;
+    	ComponentController.goTop(this);
+    	ID = RandomID.makeID(this);
+    	area = new Rect(w,h);
+    	lt=new Point(x,y);
+    	rb=new Point(x+w,y+h);
+    }
+    public static class Compare implements Comparator<Component> {
+		@Override
+		public int compare(Component arg0, Component arg1) {
+			// TODO Auto-generated method stub
+			int z1 = arg0.z;
+			int z2 = arg1.z;
+			return z1 < z2 ? -1 : z1 > z2 ? 1:0;
+		}
+	}
+	abstract void action() ;
 }
